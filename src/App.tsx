727cb8299ba991fcbf168bdae0d4824671303c14
Svetlana-AS <astaninasv1@gmail.com>
2023-04-10 15:24:53 +0200

    5-lesson-conditional-rendering

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import './App.css';
+import Accordion from "./componens/Accordion/Accordion";
+import {Rating} from "./componens/Rating/Rating";
+
+function sum () {
+
+    alert('Hello IT- Incubator')
+}
+
+
+
+
+function App() {
+    console.log("App rendering")
+    return (
+        <div>
+
+        <Accordion titleValue={"Меню"} collapced={true}/>
+        <Accordion titleValue={"Сайт бар"} collapced={false}/>
+        <Rating value={1}/>
+        <Rating value={2}/>
+        <Rating value={3}/>
+        <Rating value={4}/>
+        <Rating value={5}/>
+
+
+        </div>
+    );
+}
+type  PageTitlePropsType = {
+    title : string
+}
+function PageTitle(props:PageTitlePropsType) {
+    console.log("PageTitle rendering")
+    return <h1> {props.title}</h1>
+}
+
+
+export default App;
---
+++ src/componens/Accordion/Accordion.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+type  AccordionPropsType = {
+    titleValue : string
+    collapced : boolean
+}
+function Accordion (props : AccordionPropsType) {
+
+         return <div>
+             <AccordionTitle title ={props.titleValue}/>
+             {!props.collapced && <AccordionBody/>}
+         </div>
+}
+type  AccordionTitlePropsType = {
+    title : string
+}
+function AccordionTitle (props : AccordionTitlePropsType){
+    debugger
+    console.log("AccordionTitle rendering")
+    return <h3>{props.title}</h3>
+}
+function AccordionBody() {
+    console.log("AccordionBody rendering")
+    return <div>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    </div>
+}
+export default Accordion;
